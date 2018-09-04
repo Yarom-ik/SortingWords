5abc3107b62843654238a2cf6678e46a55b841d0
Yarom <dom2kb1@yandex.ru>
2018-09-04 08:56:51 +0300

    complete

---
+++ SortingWords/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SortingWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = null;
+            string output = null;
+            //Console.WriteLine(args[0]);
+            try
+            {
+                input = args[0];
+                output = args[1];
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + " Enter path ");
+            }
+
+            Dictionary<string, int> wordsAll = new Dictionary<string, int>();
+            try
+            {
+                StreamReader file = new StreamReader(input, Encoding.Default);
+                string s;
+                string[] str;
+                while ((s = file.ReadLine()) != null)
+                {
+                    //str = s.Split(new char[] { ' ', '?', '!', ':', '@' }, StringSplitOptions.RemoveEmptyEntries);
+                    str = Regex.Split(s, @"\W+");
+                    foreach (string s1 in str)
+                    {
+                        string j = s1.ToLower();
+
+                        if (!wordsAll.ContainsKey(j))
+                        {
+                            wordsAll.Add(j, 1);
+                        }
+                        else
+                            wordsAll[j]++;
+
+                    }
+                }
+                file.Close();
+            }
+            
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            wordsAll = wordsAll.OrderBy(k => k.Key).ToDictionary(process => process.Key, process => process.Value);
+
+            Dictionary<string, IEnumerable<KeyValuePair<string, int>>> wordsGroup = new Dictionary<string, IEnumerable<KeyValuePair<string, int>>>();
+
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                var filteredWordsGroup = wordsAll.Where(item => item.Key.StartsWith(i.ToString()));
+                var filteredWordsOrder = filteredWordsGroup.OrderByDescending(pair => pair.Value).ToArray();
+                if (filteredWordsOrder.Length > 0)
+                {
+                    wordsGroup.Add(i.ToString(), filteredWordsOrder);
+                }
+            }
+
+            try
+            {
+                StreamWriter fileOut = new StreamWriter(output, false, Encoding.Default);
+                foreach (var group in wordsGroup)
+                {
+                    fileOut.WriteLine("\r\n" + group.Key + "\r\n");
+                    foreach (var word in group.Value)
+                    {
+                        fileOut.WriteLine(word.Key + " " + word.Value);
+                    }
+
+                }
+                fileOut.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+    
+
---
+++ SortingWords/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("SortingWords")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SortingWords")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("cf484ed7-090b-4743-8cea-756f6f3e2474")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
